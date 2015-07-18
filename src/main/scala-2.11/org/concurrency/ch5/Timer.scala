450c9fce9e2eb738a0a129eafb3b7bf829d89c2b
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2015-07-18 10:45:40 +0200

    Added the ability to measure the average time of more than one execution

--- src/main/scala-2.11/org/concurrency/ch5/Timer.scala
+++ src/main/scala-2.11/org/concurrency/ch5/Timer.scala
@@ -3,15 +3,15 @@ package org.concurrency.ch5
 object Timer {
   @volatile var dummy: Any = _
 
-  def timed[T](body: => T): Double = {
+  def timed[T](body: => T, times:Int = 1): Double = {
     val start = System.nanoTime
-    dummy = body
+    for(_ <- 0 until times) dummy = body
     val end = System.nanoTime
-    ((end - start) / 1000) / 1000.0
+    (((end - start) / 1000) / 1000.0) / times
   }
 
-  def warmedTimed[T](n: Int = 200)(body: => T):Double = {
+  def warmedTimed[T](n: Int = 200, times: Int = 1)(body: => T):Double = {
     for(_ <- 0 until n) body
-    timed(body)
+    timed(body, times)
   }
 }
