cfad38b45b6394de85faf8558f4f36600b408e98
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2015-09-05 19:41:06 +0200

    Exercise 4 from chapter 6 - solution with no subjects

--- src/main/scala-2.11/org/concurrency/ch6/Signal.scala
+++ src/main/scala-2.11/org/concurrency/ch6/Signal.scala
@@ -1,43 +1,33 @@
 package org.concurrency.ch6
 
-import rx.lang.scala.{Subscription, Observable}
-import rx.lang.scala.subjects.BehaviorSubject
+import rx.lang.scala.Observable
 
-trait Signal[T] {
-  abstract def bs: BehaviorSubject[T]
-  abstract def subs: Subscription
+class Signal[T](val ob: Observable[T]) {
+  var v: T = _
+  ob.subscribe(v => this.v = v)
 
-  def apply(): T = {
-    bs.getValue
-  }
-  def map[S](f: T => S): Signal[S] = {
-    val oldbs = bs
-    new Signal[S] {
-      def bs = BehaviorSubject[S]()
-      def subs = oldbs.map(f).subscribe(bs)
-    }
-  }
-  def zip[S](that: Signal[S]): Signal[(T,S)] = {
-    val oldbs = bs
-    new Signal[(T,S)] {
-      def bs = BehaviorSubject[(T,S)]()
-      def subs = oldbs.zip(that.bs).subscribe(bs)
-    }
-  }
-  def scan[S](z: S)(f:(S,T) => S): Signal[S] = {
-    val oldbs = bs
-    new Signal[S] {
-      def bs = BehaviorSubject[S]()
-      def subs = oldbs.scan(z)(f).subscribe(bs)
-    }
-  }
+  def apply(): T = v
+  def map[S](f: T => S): Signal[S] = new Signal(ob.map(f))
+  def zip[S](that: Signal[S]): Signal[(T,S)] = new Signal(ob.zip(that.ob))
+  def scan[S](z:S)(f: (S,T) => S) = new Signal(ob.scan(z)(f))
 }
+
 
 object SignalApp extends App {
   implicit final class SignalExtensions[T](val source: Observable[T]) {
-    def toSignal:Signal[T] = new Signal[T] {
-      def bs = BehaviorSubject[T]()
-      def subs = source.subscribe(bs)
-    }
+    def toSignal:Signal[T] = new Signal(source)
   }
+
+  val s = Observable.from(List("foo", "bar")).toSignal
+  println(s())
+
+  val s1 = Observable.from(List("0", "1", "2", "3")).toSignal.map(_.toInt)
+  println(s1())
+
+  val s2 = Observable.from(List("zero", "one", "two")).toSignal
+  val s3 = Observable.from(List("0", "1", "2")).toSignal
+  println(s2.zip(s3)())
+
+  val s4 = s1.scan(0)(_+_)
+  println(s4())
 }
