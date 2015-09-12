ed2cb70f8fe8145a943bb8bedb9e1bdce31ef383
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2015-09-12 10:57:28 +0200

    Deleted unnecessary timeout.

---
+++ src/main/scala-2.11/org/concurrency/ch7/AtomicRollbackCount.scala
@@ -0,0 +1,55 @@
+package org.concurrency.ch7
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.stm._
+import scala.concurrent.{Future, blocking}
+
+object AtomicRollbackCount {
+  def count[T](block: InTxn => T): (T, Int) = {
+    val count = new AtomicInteger(-1)
+    atomic {
+      implicit tx => {
+        count.incrementAndGet()
+        (block(tx), count.get())
+      }
+    }
+  }
+}
+
+object AtomicRollbackCountApp extends App {
+
+  val mv1 = new MVar[Boolean]()
+  val mv2 = new MVar[Boolean]()
+  val block = (tx: InTxn) => atomic {
+    implicit tx => {
+      if (mv1.take && mv2.take) "end"
+      else retry
+    }
+  }
+
+  Future {
+    blocking {
+      println(s"${AtomicRollbackCount.count(block)}")
+    }
+  }
+
+  Thread.sleep(1000)
+
+  Future {
+    atomic {
+      implicit tx => {
+        mv1.put(true)
+      }
+    }
+
+    atomic {
+      implicit tx => {
+        mv2.put(true)
+      }
+    }
+  }
+
+  Thread.sleep(2000)
+}
